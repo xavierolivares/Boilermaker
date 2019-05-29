991f9ebe72e475dfc09391829f23e1b9be829d7b
Xavier Olivares <xm.olivares@gmail.com>
2019-05-29 17:27:18 -0400

    added error handling if api/route doesnt exist when queried by client

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -4,4 +4,10 @@ router.use('/users', require('./users'))
 router.use('/puppies', require('./puppies'))
 router.use('/kittens', require('./kittens'))
 
+router.use((req,res,next) => {
+    const err = new Error('Not found.')
+    err.status(404)
+    next(err)
+})
+
 module.exports = router
