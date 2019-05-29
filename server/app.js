b302ed10ca4d0b68b5a5675116c0ec06afdff8d3
Xavier Olivares <xm.olivares@gmail.com>
2019-05-29 17:30:44 -0400

    added 500 error handling and port listening to app.js

--- server/app.js
+++ server/app.js
@@ -15,3 +15,20 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api', require('./apiRoutes'))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './path/to/index.html');
+  });
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
+app.listen(port, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+});
