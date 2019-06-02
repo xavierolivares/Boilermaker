95af94f3629c2485615a49db34bfde7fe8c11265
Xavier Olivares <xm.olivares@gmail.com>
2019-06-02 08:33:35 -0400

    configured the db setup wrong, didnt need to add port number, tip: look at error and google

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:3000/appy', {
+const db = new Sequelize('postgres://localhost/appy', {
   logging: false // unless you like the logs
   // ...and there are many other options you may want to play with
 });
